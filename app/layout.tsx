af9b2ca17e3de94322aee1f6f52ffb615bf66d7f
Nishadchowdhury <Nishadhj111@gmail.com>
2024-05-04 23:34:49 +0600

    all done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
-
+import { ThemeProvider } from "@/providers/theme-providers";
+import { ThemeToggle } from "@/components/custom/themeToggle";
+import { Toaster } from "@/components/ui/toaster";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -20,7 +22,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn( "min-h-screen bg-background font-sans antialiased", fontSans.variable)}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+
+        >
+          <div className="h-16 border-b border-slate-600 flex items-center justify-end  px-3 ">
+            <ThemeToggle />
+          </div>
+
+          {children}
+        </ThemeProvider>
+        <Toaster />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import TableComponent from "./(table)/TableComponent";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Button
-     variant={'destructive'}
-     size={"icon"}
-     
-     >
-      Get
-     </Button>
+    <main className=" min-h-screen flex-col items-center justify-between lg:px-24 md:px-10 px-4">
+     <TableComponent />
     </main>
   );
 }
